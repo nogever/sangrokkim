425bf141e52da65a8e045c6a1897485ae14b75df
Sean Kim <nogever@gmail.com>
2017-11-16 14:48:48 -0500

    Add snake

---
+++ src/components/resumes/snake/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="snake">
+    Snake game
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sr-snake',
+}
+</script>
+
+<style lang="scss" scoped>
+.snake {
+  border: 6px solid white;
+  width: 100%;
+  height: 100%;
+}
+</style>
