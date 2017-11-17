16c7393abdacd3e161a06dc64206926bb346e2ef
Sean Kim <nogever@gmail.com>
2017-11-17 15:51:38 -0500

    Add background change animation

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="hello" v-stream:mousemove="mouse$" v-bind:style="{ background: `hsla(${coor && coor.x + coor.y}, 67.4%, 73.5%, 1)` }">
     <h1>creative web developer</h1>
     <sr-contact></sr-contact>
   </div>
@@ -7,22 +7,37 @@
 
 <script>
 import SrContact from './Contact'
+import Rx from 'rxjs/Rx'
+
 export default {
   name: 'sr-hello',
   components: { SrContact },
+  subscriptions () {
+    this.mouse$ = new Rx.Subject()
+
+    return {
+      coor: this.mouse$.throttleTime(1000).map(e => {
+        return {
+          x: e.event.clientX,
+          y: e.event.clientY,
+        }
+      }),
+    }
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scroped>
+<style lang="scss" scoped>
 .hello {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
-  background: rgba(142, 206, 233, 1);
+  /*background: rgba(142, 206, 233, 1);*/
+  background: hsla(198, 67.4%, 73.5%, 1);
   padding: 100px 6%;
+  transition: background 1s ease-in;
   h1 {
     margin: 48px;
   }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Rx from 'rxjs/Rx'
+import VueRx from 'vue-rx'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRx, Rx)
 
 /* eslint-disable no-new */
 new Vue({
