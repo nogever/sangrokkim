47eef73ae4ac82a7752c751348bd6d21bb67d680
Sean Kim <nogever@gmail.com>
2018-01-31 22:58:38 -0500

    Change link style and add global scss file

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -5,9 +5,10 @@
     </div>
     <div class="snake">
       <router-link to="/games/snake">
-        <svg viewBox="0 0 36 36">
+        Snake
+        <!-- <svg viewBox="0 0 36 36">
           <path d="M34,18v1v1h-2h-5v-9h-8v3h-4v-3H9v9H3v1h31v1H2v-3h6v-9h8v3h2v-3h10v9h4v-1H34z M17,28h2v-2h-2V28z"/>
-        </svg>
+        </svg> -->
       </router-link>
     </div>
   </div>
